99d42ce21210ff6fb7b191302a3a76c557a6fb8d
sofiateixeiraa <48253845+sofiateixeiraa@users.noreply.github.com>
2019-11-05 00:55:12 +0000

    Add files via upload

---
+++ Ficheiros/src/main.c
@@ -0,0 +1,19 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+
+int main(void)
+{
+  //fazer as inicializações necessárias
+
+
+  while (1)
+  {
+    //código que vai correr em loop
+
+  }
+
+
+}
+
